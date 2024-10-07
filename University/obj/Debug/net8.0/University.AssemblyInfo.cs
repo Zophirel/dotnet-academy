b6e6d7cb263db88b31b9ae87278cdd8d750e6bb0
Francesco <sworld.design@gmail.com>
2024-10-07 10:57:11 +0200

    added data

--- University/obj/Debug/net8.0/University.AssemblyInfo.cs
+++ University/obj/Debug/net8.0/University.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("University")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+528c770b254640e39b16b23a47acadd51452f545")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ed5c3c98fa6b9c924320348a38dc3c59b45d20c")]
 [assembly: System.Reflection.AssemblyProductAttribute("University")]
 [assembly: System.Reflection.AssemblyTitleAttribute("University")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
